0ac3f07765e0fea3bfc416eec83966a97d1275bf
corner_coder <56599360+fatihohn@users.noreply.github.com>
2025-11-22 12:40:58 +0900

    update certbot to use cloudflare token

--- certbot/docker-entrypoint.sh
+++ certbot/docker-entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 mkdir -p /etc/cloudflare && \
-# echo "dns_cloudflare_email = $EMAIL_CERTIFICATOR" > /etc/cloudflare/cloudflare.ini && \
 echo "dns_cloudflare_api_token = $CLOUDFLARE_API_TOKEN" >> /etc/cloudflare/cloudflare.ini && \
 chmod 600 /etc/cloudflare/cloudflare.ini
 
